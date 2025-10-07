61b6b99d97848ae998adc1c280df03efd61d8690
Emma Luk <emma07315@gmail.com>
2025-10-07 14:26:43 -0700

    collections

---
+++ Collections/dictionary.py
@@ -0,0 +1,14 @@
+# # California State Symbols
+# stateBird = 'California Quail'
+# stateAnimal = 'Grizzly Bear'
+# stateFlower = 'California Poppy'
+# stateFruit = 'Avocado'
+
+california_symbols = {
+    'Bird' : 'California Quail',
+    'Animal' : 'Grizzly Bear',
+    'Flower' : 'California Poppy',
+    'Fruit' : 'Avocado'
+}
+
+print(california_symbols['Animal'])
---
+++ Collections/list.py
@@ -0,0 +1,16 @@
+# note: a python list can contain vairous type, e.g. [1, 'one', true]
+# you can add and remove items or change an existing item (i.e. mutable)
+
+# city1 = "pleasanton"
+# city2 = "san ramon"
+# city3 = "livermore"
+# city4 = "flavor town"
+
+cities = [
+    "pleasanton",
+    "san ramon",
+    "livermore",
+    "flavor town"
+]
+
+print(cities[1])
