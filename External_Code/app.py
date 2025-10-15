697d036217e0b5bbb02330faff30651df69f2ab4
Emma Luk <emma07315@gmail.com>
2025-10-14 20:52:47 -0600

    external code

---
+++ External_Code/app.py
@@ -0,0 +1,3 @@
+import module as m
+
+m.mult(2, 3)
---
+++ External_Code/module.py
@@ -0,0 +1,6 @@
+# module: python file that contains code, like variables or functions
+# package/library: using multiple modules together so they are distributed and used in a group
+# framework: when a set of code is not just used together but used in a specifc way
+
+def mult(x, y):
+    print(f'{x} * {y} = {x*y}')
